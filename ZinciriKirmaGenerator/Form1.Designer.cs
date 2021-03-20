e0d36fa9a01526fb9bd604ccb6ff285597f80458
MustafaSamedYeyin <mustafasamedyeyin@gmail.com>
2021-03-20 10:54:57 +0300

    Add project files.

---
+++ ZinciriKirmaGenerator/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace ZinciriKirmaGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Başlangıç Tarihi :";
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpStart.Location = new System.Drawing.Point(174, 36);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(126, 26);
+            this.dtpStart.TabIndex = 1;
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpEnd.Location = new System.Drawing.Point(174, 82);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(126, 26);
+            this.dtpEnd.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bitiş Tarihi:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 26);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Olustur.";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(260, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(243, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mustafa Samed tarafından yaratılmışır.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 164);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dtpEnd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtpStart);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ZinciriKirmaGenerator/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZinciriKirmaGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var path = openFileDialog1.ShowDialog();
+            var fullpath = openFileDialog1.InitialDirectory + openFileDialog1.FileName;
+            var html = "<!DOCTYPE html><html lang=\"en\"><head>    <meta charset=\"UTF-8\">    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">    <title>Document</title></head><body>    <table style=\"margin: auto auto;border: 3px solid rgba(33, 77, 158, 0.472);border-collapse: collapse;  \"> ";
+            var günFark =(dtpEnd.Value - dtpStart.Value).Days+1;
+            var ustLoop = günFark / 12;
+            var günce = dtpStart.Value;
+
+            for (int i = 0; i < ustLoop; i++)
+            {
+                html += "<tr  style=\"border: 1px solid red;\">";
+                for (int K = 0; K < 12; K++)
+                {
+                    html += " <th style=\"border: 2px solid red;\">";
+                    html += "<div class=\"tarih\" onclick=\"tikle(this)\" style=\"min-height: 45px; min-width: 49px; position: relative ;\">";
+                    var gün = günce.Day;
+                    var ayy = günce.Month;
+                    var yıl = günce.Year;
+                    html += "<span style=\"color: red; font-size: 10px; position: absolute;top: 0;right: 0; \">"+gün+"</span>";
+                    html += "<span style=\"color: turquoise; font-size: 10px;  position: absolute; bottom: 0; left: 0;\">" + ayy + "</span>";
+                    html += "<span style=\"color: #53354a; font-size: 10px; position: absolute; bottom: 0; right: 0; \">" + yıl + "</span>";
+                    günce = günce.AddDays(1);
+                    
+
+                    html += "</div>";
+                }
+                html += "</tr>";
+
+            }
+
+            for (int i = 0; i < günFark % 12; i++)
+            {
+                    html += " <th style=\"border: 2px solid red;\">";
+                    html += "<div class=\"tarih\" onclick=\"tikle(this)\" style=\"min-height: 45px; min-width: 49px; position: relative ;\">";
+                    var gün = günce.Day;
+                    var ayy = günce.Month;
+                    var yıl = günce.Year;
+                    html += "<span style=\"color: red; font-size: 10px; position: absolute;top: 0;right: 0; \">" + gün + "</span>";
+                    html += "<span style=\"color: turquoise; font-size: 10px;  position: absolute; bottom: 0; left: 0;\">" + ayy + "</span>";
+                    html += "<span style=\"color: #53354a; font-size: 10px; position: absolute; bottom: 0; right: 0; \">" + yıl + "</span>";
+                    günce = günce.AddDays(1);
+                    html += "</div>";
+                
+            }
+            html += "<script>";
+            html += "  function tikle(a) {";
+            html += "if (!a.innerHTML.includes('<em style=\"font-size:35px;\">✓</em>')) {";
+            html += "a.innerHTML += '<em style=\"font-size:35px;\">✓</em>';";
+            html += "}";
+            html += "else {";
+            html += "var yenisi = a.innerHTML.replace(\"<em style=\"font-size:35px;\" >✓</ em >\", \"\");";
+            html += " a.innerHTML = yenisi;";
+            html += "}        }    </script>";
+            html += "</table></body></html>";
+          
+            File.WriteAllText(fullpath, html);
+        }
+    }
+}
